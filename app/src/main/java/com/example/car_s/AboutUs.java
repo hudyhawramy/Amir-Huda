3a799d3eaef739423a7d18c738c32ec44e2bee0c
Graduation-Project-CS <79712720+Graduation-Project-CS@users.noreply.github.com>
2021-04-09 21:34:54 +0300

    party taza 3

--- app/src/main/java/com/example/car_s/AboutUs.java
+++ app/src/main/java/com/example/car_s/AboutUs.java
@@ -1,18 +1,14 @@
 package com.example.car_s;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Setting extends AppCompatActivity {
+public class AboutUs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting);
-    }
-
-    public void submitchange(View view) {
+        setContentView(R.layout.about_us);
     }
 }
--- app/src/main/java/com/example/car_s/Navigation.java
+++ app/src/main/java/com/example/car_s/Navigation.java
@@ -1,5 +1,6 @@
 package com.example.car_s;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -40,7 +41,7 @@ public class Navigation extends AppCompatActivity implements NavigationView.OnNa
         navigationView.setNavigationItemSelectedListener(this);
 
 
-
+        navigationView.setCheckedItem(R.id.nav_home);
 
     }
 
@@ -56,6 +57,27 @@ public class Navigation extends AppCompatActivity implements NavigationView.OnNa
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_profile:
+                Intent intent = new Intent(Navigation.this, Setting.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_aboutus:
+                Intent intent1 = new Intent(Navigation.this, AboutUs.class);
+                startActivity(intent1);
+                break;
+            case R.id.nav_previos:
+                Intent intent2 = new Intent(Navigation.this, Previos.class);
+                startActivity(intent2);
+                break;
+
+        }
+
+
+        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 }
